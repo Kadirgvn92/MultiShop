6357928bdfb1f53ae1f6e9a1df916f1d1acd01d8
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-14 08:50:47 +0300

    Lesson 8 : ProductDetails classes created

--- Services/Catalog/MultiShop.Catalog/Entities/Product.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -4,14 +4,14 @@ namespace MultiShop.Catalog.Entities;
 
 public class Product
 {
-    [BsonId]
-    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    [BsonId]  //BsonId attribute entitiylerde ıd belirlemek için kullanılır mongodbde
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)] //bu attribute mongoda objectıd eşsiz olmasını sağlar
     public string ProductID { get; set; }
     public string ProductName { get; set; }
     public decimal ProductPrice { get; set; }
     public string ProductImageUrl { get; set; }
     public string ProductDescription { get; set; }
     public string CategoryId { get; set; }
-    [BsonIgnore]
+    [BsonIgnore] //BsonIgnore ise category nesnesini görmezden gelmesini sağlar bu sayede Category nesnesi db de boşuna yer kaplamaz
     public Category Category { get; set; }
 }
--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -1,5 +1,10 @@
-﻿namespace MultiShop.Catalog.Entities;
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities;
 
 public class ProductDetail
 {
+    public string ProductDetailId { get; set; }
+    public string ProductDescription { get; set; }
+    public string ProductInfo { get; set; }
 }
