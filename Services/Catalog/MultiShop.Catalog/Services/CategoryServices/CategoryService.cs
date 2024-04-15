c01cd55ae46ea3028002e3d00370272883e34ddf
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-15 22:00:08 +0300

    Lesson 15 : Category service created

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.DTOs.CategoryDTOs;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Services.CategoryServices;
+
+public class CategoryService : ICategoryService
+{
+    private readonly IMongoCollection<Category> _categoryCollection;
+    private readonly IMapper _mapper;
+    public Task CreateCategoryAsync(CreateCategoryDTO createCategoryDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteCategoryAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ResultCategoryDTO>> GetAllCategoryAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetByIdCategoryDTO> GetByIdCategoryAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCategoryAsync(UpdateCategoryDTO updateCategoryDTO)
+    {
+        throw new NotImplementedException();
+    }
+}
