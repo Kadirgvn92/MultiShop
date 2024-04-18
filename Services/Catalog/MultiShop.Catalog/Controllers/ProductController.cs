79ceb3f2dffd277987afde2b0c90e1565c8b6e96
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-18 22:45:26 +0300

    Lesson 21 - 2 : ProductController completed

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.DTOs.ProductDTOs;
+using MultiShop.Catalog.Services.ProductServices;
+
+namespace MultiShop.Catalog.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _ProductService;
+    public ProductController(IProductService ProductService)
+    {
+        _ProductService = ProductService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> ProductList()
+    {
+        var values = await _ProductService.GetAllProductAsync();
+        return Ok(values);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProductById(string id)
+    {
+        var values = _ProductService.GetByIdProductAsync(id);
+        return Ok(values);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(CreateProductDTO createDTO)
+    {
+        await _ProductService.CreateProductAsync(createDTO);
+        return Ok("Ürün başarıyla eklendi");
+    }
+    [HttpDelete]
+    public async Task<IActionResult> DeleteProduct(string id)
+    {
+        await _ProductService.DeleteProductAsync(id);
+        return Ok("Ürün başarıyla silindi");
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateProduct(UpdateProductDTO updateDTO)
+    {
+        await _ProductService.UpdateProductAsync(updateDTO);
+        return Ok("Ürün başarıyla güncellendi.");
+    }
+}
