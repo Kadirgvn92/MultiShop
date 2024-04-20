8a8133cc1b91f13a4eac1495082b7da57ccf8b50
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-20 17:06:42 +0300

    Lesson 26 : Discount API project and some entities created

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MultiShop.Discount.Context;
+
+public class DapperContext : DbContext
+{
+}
---
+++ Services/Discount/MultiShop.Discount/Entities/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Discount.Entities;
+
+public class Coupon
+{
+    public int CouponId { get; set; }
+    public string Code { get; set; }
+    public int Rate { get; set; }
+    public bool isActive { get; set; }
+    public DateTime ValidDate { get; set; }
+}
